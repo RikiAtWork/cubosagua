067ddaf59edd5ff5e9773e4f5a111966de89b270
Ricardo U <ricardoulloaaguirre@gmail.com>
2023-05-10 21:13:59 +0200

    Fichero cubosagua con vaciado de todos y readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MAFF
+# RUA
 
 CFGS Desenrotllament d'Aplicacions Web
 
@@ -8,3 +8,7 @@ Entorns de Desenrotllament
 
 Programa exemple per activitat de Github amb Python
 
+## FECHA: 10/05/2023 21:10
+He añadido a la funcion selecOpcion la nueva opcion de vaciar y he añadido el caracter 'B' en la comprobacion del if.
+
+Por ultimo en la funcion de jugar() he añadido la opcion de la letra y he pasado como parametros a la funcion de vaciar() los cubos 8, 5 y 3. 
--- cubosagua.py
+++ cubosagua.py
@@ -63,6 +63,7 @@ class juegoCubos:
         print('  (L)lenar un cubo')
         print('  (V)aciar un cubo')
         print('  (M)over el agua de un cubo a otro')
+        print('  (B)Vaciar todos los cubos')
         print('  (S)alir')
 
         while True:
@@ -71,7 +72,7 @@ class juegoCubos:
                 print('Gracias por jugar!')
                 sys.exit()
 
-            if move in ('L', 'V', 'M'):
+            if move in ('L', 'V', 'M','B'):
                 return move
 
     def selecCubo(self, mensaje):
@@ -118,6 +119,13 @@ class juegoCubos:
                 cuboOrigen = self.selecCubo('Selecciona el cubo ORIGEN 8, 5, 3 o SALIR:')
                 cuboDestino = self.selecCubo('Selecciona el cubo DESTINO 8, 5, 3 o SALIR:')
                 self.moverCubo(cuboOrigen, cuboDestino)
+            elif opcion == "B":
+                cubo = '8'
+                self.vaciarCubo(cubo)
+                cubo = '5'
+                self.vaciarCubo(cubo)
+                cubo = '3'
+                self.vaciarCubo(cubo)
             self.mostrarEstadoCubos()
             self.checkObjetivo()
 
